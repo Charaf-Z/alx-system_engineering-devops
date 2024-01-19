0ddd9bc43789c54ebe19509c31b7223622261ff6
Charaf-Z <charafomarzitouni@gmail.com>
2024-01-19 12:10:37 +0100

    puppet-lint for 1-file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
 # Install an especific version of flask (2.1.0)
 
 package {'install_flask':
-    name   => 'flask',
-    ensure => '2.1.0',
+    name     => 'flask',
+    ensure   => '2.1.0',
     provider => pip3,
-    command => '/usr/bin/pip3',
 }
