34962cc0b07131eb1f947f71efe90e6cd0c3f50e
Charaf-Z <charafomarzitouni@gmail.com>
2024-03-05 23:21:47 +0100

    update the 0-file 1.1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "alx:0x16.api.advanced:v1.0.0"}
     response = get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
