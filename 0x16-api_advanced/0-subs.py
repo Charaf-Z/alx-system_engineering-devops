c656b5bd2686018648bb3b2f0e374b468eefb03e
Charaf-Z <charafomarzitouni@gmail.com>
2024-03-05 23:23:35 +0100

    update the 0-file 1.2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module to retrieve the number of subscribers for a given subreddit."""
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
@@ -16,8 +16,9 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "alx:0x16.api.advanced:v1.0.0"}
-    response = get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
     results = response.json().get("data")
-    return results.get("subscribers")
+    nbr = results.get("subscribers")
+    return nbr
